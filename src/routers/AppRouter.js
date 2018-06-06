7809d49633263c7ade30fb5bed99d17e922d806e
m8601 <robert.szymanski08@gmail.com>
2018-06-06 19:11:36 +0200

    Add private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,35 +1,31 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import {Router, Route, Switch, Link, NavLink} from 'react-router-dom';
 
 import ExpenseDashbordComponent from './../components/ExpenseDashbordComponent';
 import AddExpensePage from './../components/AddExpensePage';
 import EditExpensePage from './../components/EditExpensePage';
 import HelpPage from './../components/HelpPage';
 import NotFoundPage from './../components/NotFoundPage';
-import Header from './../components/Header';
 
+import LoginPage from '../components/loginPage';
+import PrivateRoute from './PrivateRoute';
 
-
-
-
-
-
-
-
+export const history = createHistory();
 
 const AppRouter = () => (
-  <BrowserRouter>
+  <Router history={history}>
     <div>
-      <Header/>
       <Switch>
-        <Route path='/' component={ExpenseDashbordComponent} exact={true}/>
-        <Route path='/create' component={AddExpensePage}/>
+        <Route path='/' component={LoginPage} exact={true}/>
+        <PrivateRoute path='/dashboard' component={ExpenseDashbordComponent}/>
+        <PrivateRoute path='/create' component={AddExpensePage}/>
         <Route path='/help' component={HelpPage}/>
-        <Route path='/edit/:id' component={EditExpensePage}/>
+        <PrivateRoute path='/edit/:id' component={EditExpensePage}/>
         <Route component={NotFoundPage}/>
       </Switch>
     </div>
-  </BrowserRouter>
+  </Router>
 );
 
 export default AppRouter;
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,19 +1,23 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import authReducer from '../reducers/auth';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filter';
-import thunk from 'redux-thunk';
 
-const composeEnhancers = window.__REDUX_DEVTOOL_EXTENSION_COMPOSE__ || compose;
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default () => {
   // Store creation
   const store = createStore(
     combineReducers({
       expenses: expensesReducer,
-      filters: filtersReducer
+      filters: filtersReducer,
+      auth: authReducer
     }),
     composeEnhancers(applyMiddleware(thunk))
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+   
+
 
   );
 return store;
